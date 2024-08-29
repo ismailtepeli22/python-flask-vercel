77fec9d3578c6b87195f9cfeeebd5f502d33eaa2
İsmail Tepeli <ismail.tepeli@pqsoftware.io>
2024-08-29 16:30:18 +0300

    veercel commit

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return "<p>Hello, vercel World!</p>"
+
+@app.route("/about")
+def about():
+    return "<p>Hello, about!</p>"
 
 if __name__=="__main__":
     app.run(debug=True)
